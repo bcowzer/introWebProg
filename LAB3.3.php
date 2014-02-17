02722f77f306fcdcf75764e64a98bb9ff3c0e7ae
bcowzer <bcowzer@hotmail.com>
2014-02-17 14:32:38 +0000

    LAB3.3 commit

---
+++ LAB3.3.php
@@ -0,0 +1,11 @@
+<?php
+//seta value
+$celciusValue = 18.9;
+
+//define somewhere to keep our new value and use it
+$farenheitValue = $celciusValue * 1.8 + 32;
+
+//output the result
+print ("Celcius value of " . $celciusValue . " is " . $farenheitValue . " farenheit.");
+
+?>
